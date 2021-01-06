e61023b33585a234f7849694479cf1693eb38d00
weijiang <DISnwd$658>
2021-01-06 18:28:29 +0800

    测试mesh-sdk

--- src/main/java/com/jiangwei/springboottest/myboot/MybootApplication.java
+++ src/main/java/com/jiangwei/springboottest/myboot/MybootApplication.java
@@ -1,8 +1,10 @@
 package com.jiangwei.springboottest.myboot;
 
+import com.xiaoju.am.mesh.sdk.http.EnableJaguarClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJaguarClients(basePackages = {"com.jiangwei.springboottest.myboot.facade"})
 @SpringBootApplication
 public class MybootApplication {
 
