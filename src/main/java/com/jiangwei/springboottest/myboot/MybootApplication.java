8ea0b16b1e68230e47501d113603bc912887bed5
weijiang <weijiang@didiglobal.com>
2021-02-20 21:28:34 +0800

    修改配置文件

--- src/main/java/com/jiangwei/springboottest/myboot/MybootApplication.java
+++ src/main/java/com/jiangwei/springboottest/myboot/MybootApplication.java
@@ -1,17 +1,13 @@
 package com.jiangwei.springboottest.myboot;
 
-import com.xiaoju.am.mesh.sdk.http.EnableJaguarClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableJaguarClients(basePackages = {"com.jiangwei.springboottest.myboot.facade"})
 @SpringBootApplication
 public class MybootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybootApplication.class, args);
-
-        //UUUUUUUUUUUUUU
     }
 
 }
