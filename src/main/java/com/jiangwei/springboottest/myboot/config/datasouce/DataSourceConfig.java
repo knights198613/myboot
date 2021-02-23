9def92aaed36ea4b70fd6a454ddead45786f57b6
weijiang <weijiang@didiglobal.com>
2021-02-23 15:21:46 +0800

    添加连接池

--- src/main/java/com/jiangwei/springboottest/myboot/config/datasouce/DataSourceConfig.java
+++ src/main/java/com/jiangwei/springboottest/myboot/config/datasouce/DataSourceConfig.java
@@ -2,38 +2,37 @@ package com.jiangwei.springboottest.myboot.config.datasouce;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author: weijiang
  * @date: 2021/2/23
  * @description:
  **/
-@ConditionalOnClass(HikariDataSource.class)
-@ConditionalOnMissingBean(DataSource.class)
-@ConditionalOnProperty(name="datasource.type", havingValue = "com.zaxxer.hikari.HikariDataSource", matchIfMissing = true)
+@Configuration
 public class DataSourceConfig {
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikaricp")
-    public HikariDataSource createDatasource(DataSourceProperties dataSourceProperties) {
+    @Bean("hikariConfig")
+    @ConfigurationProperties(prefix = "hikaricp")
+    public HikariConfig createConfig() {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dataSourceProperties.getUrl());
-        config.setPassword(dataSourceProperties.getPassword());
-        config.setUsername(dataSourceProperties.getUsername());
-        config.setDriverClassName(dataSourceProperties.getDriverClassName());
-        HikariDataSource dataSource = new HikariDataSource(config);
-
-        return dataSource;
-
+        return config;
     }
+
+    @Bean
+    public HikariDataSource createDatasource(DataSourceProperties dataSourceProperties, HikariConfig hikariConfig) {
+        hikariConfig.setJdbcUrl(dataSourceProperties.getUrl());
+        hikariConfig.setPassword(dataSourceProperties.getPassword());
+        hikariConfig.setUsername(dataSourceProperties.getUsername());
+        hikariConfig.setDriverClassName(dataSourceProperties.getDriverClassName());
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        return dataSource;
+    }
+
+
 
 
 }
